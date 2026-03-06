d4b0aac1fcb547ace6ab70ec37b0e999125f61e2
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-03-06 16:21:20 -0300

    Creating the tables of my project.

---
+++ DATABASE/pets_database.sql
@@ -0,0 +1,17 @@
+-- Criação de conta do cliente
+create table clientPets_account();
+-- Ong's
+create table pets_ongs();
+-- Denúncia de animais perdidos
+create table animal_perdido_denuncia();
+-- Serviço de adoção
+create table adocao();
+-- Serviços oferecidos 
+create table cuidador();
+create table passeador();
+create table hospedagem();
+create table hotel_pet();
+create table creche();
+create table veterinario();]
+create table banho_tosa();
+create table adestrador();
