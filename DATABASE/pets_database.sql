871f87ea17a77f56e7fd3d051175ee5e3cfa9348
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-03-07 11:25:15 -0300

    Add more datas inside the tables.

--- DATABASE/pets_database.sql
+++ DATABASE/pets_database.sql
@@ -1,18 +1,69 @@
 -- Criação de conta do cliente
-create table clientPets_account();
+create table clientPets_account(
+	id_clientAccount int auto_increment primary key,
+	nome varchar(200),
+	email varchar(250),
+	senha varchar(70),
+	cpf varchar(25)
+);
 -- Ong's
-create table pets_ongs();
+create table pets_ongs(
+	id_pets_ongs int auto_increment primary key,
+	nome_ong varchar(150),
+	chave_pix varchar(),
+	cnpj varchar(),
+	telefone varchar(),
+	WhatsApp varchar(),
+	rede_instagram varchar()
+);
 -- Denúncia de animais perdidos
-create table animal_perdido_denuncia();
+create table animal_perdido_denuncia(
+	id_denuncia int auto_increment primary key,
+	tipo_animal enum("Cachorro", "Gato", "Passaro", "Ave", "Outros"),
+	urgencia enum("Baixa", "Média", "Alta", "Crítica - Risco de vida");
+	descricao text,
+	localizacao 
+	cidade
+	telefone varchar(30),
+	foto bytea -- This kinda of data storage a photo.
+);
 -- Serviço de adoção
-create table adocao();
+create table adocao(
+	id_animal int auto_increment primary key,
+	name_animal varchar(100),
+	raca_animal varchar(),
+	idade_animal enum("Filhote", "Jovem", "Adulto", "Idoso"),
+	sexo enum("Macho", "Fêmea"),
+	catastracao enum("Castrado", "Não-castrado"),
+	vacinacao enum("Vacinado", "Não-vacinado"),
+	porte varchar("Pequeno", "Médio", "Grande"),
+	sobre text,
+	estado varchar(150),
+	telefone varchar(20)
+);
 -- Serviços oferecidos 
-create table cuidador();
-create table passeador();
-create table hospedagem();
-create table hotel_pet();
-create table creche();
-create table veterinario();
-create table banho_tosa();
-create table adestrador();
+create table cuidador(
+	id_cuidador int auto_increment primary key,
+);
+create table passeador(
+	id_passeador int auto_increment primary key,
+);
+create table hospedagem(
+	id_hospedagem int auto_increment primary key,
+);
+create table hotel_pet(
+	id_hotelPet int auto_increment primary key,
+);
+create table creche(
+	id_creche int auto_increment primary key,
+);
+create table veterinario(
+	id_veterinario int auto_increment primary key,
+);
+create table petShop(
+	id_petShop int auto_increment primary key,
+);
+create table adestrador(
+	id_adestrador int auto_increment primary key,
+);
 -- Outros dados
